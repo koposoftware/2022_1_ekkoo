61004c50a8e2642adfe2a877f7bda53ed8853368
92kyoung <100885842+92kyoung@users.noreply.github.com>
2022-10-23 19:10:06 +0900

    Delete CoolSmsTest.java

--- final-hana1qurator/src/main/java/kr/ac/kopo/util/CoolSmsTest.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.kopo.util;
-
-public class CoolSmsTest {
-public static void main(String[] args) {
-	MessageService service = new MessageService();
-	service.sendMessage("01020617815", "하이");
-}
-}
